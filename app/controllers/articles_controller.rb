dc36d30a7b5d314afb33e3fe7391bd0e080981e1
Julien VENNIN <julien.vennin@gmail.com>
2019-08-13 17:31:57 +0200

    Wikiminous

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  def index         # GET /articles
+    @articles = Article.all
+  end
+
+  def show          # GET /articles/:id
+    # @task = Article.find(params[:id])
+  end
+
+  def new           # GET /articles/new
+    @article = Article.new
+  end
+
+  def create       # POST /articles
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path(@article)
+  end
+
+  def edit          # GET /articles/:id/edit
+    # @article = Article.find(params[:id])
+  end
+
+  def update        # PATCH /articles/:id
+    # @article = article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_path(@article)
+  end
+
+  def destroy       # DELETE /articles/:id
+    # @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
